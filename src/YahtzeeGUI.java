213474d15714d2261142fe83ad95b0a1e074483f
Bill Jojo <jojowil@gmail.com>
2024-12-22 21:10:40 -0500

    more cleanup, formatting, docs. added setPrompts for shadow values in TextFields. Added isGameOver.

--- src/YahtzeeGUI.java
+++ src/YahtzeeGUI.java
@@ -11,6 +11,7 @@ import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.geometry.HPos;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -19,13 +20,13 @@ public class YahtzeeGUI extends Application {
     // All the beautiful labels!
     Label rollsLeftLBL, diceTotalLBL, subtotalLBL, bonusLBL, upperTotalLBL, totalLBL;
     ArrayList<Label> labels = new ArrayList<>();
-    
+
     // And, of course, our essential text fields!
     TextField rollsLeftTF, diceTotalTF, onesTF, twosTF, threesTF, foursTF, fivesTF, sixesTF,
             threeOKTF, fourOKTF, fullHouseTF, smStrTF, lgStrTF, yahtzeeTF, chanceTF,
             subtotalTF, bonusTF, upperTotalTF, totalTF;
     ArrayList<TextField> textFields = new ArrayList<>();
-            
+
     // But where would we be without our buttons?
     Button rollBTN, onesBTN, twosBTN, threesBTN, foursBTN, fivesBTN, sixesBTN,
             threeOKBTN, fourOKBTN, fullHouseBTN, smStrBTN, lgStrBTN, yahtzeeBTN, chanceBTN;
@@ -41,6 +42,7 @@ public class YahtzeeGUI extends Application {
 
     /**
      * Create a scoring Button and add it to the list.
+     *
      * @param n name of button
      * @param w max width of the button
      * @return actioned button object
@@ -55,6 +57,7 @@ public class YahtzeeGUI extends Application {
 
     /**
      * Create a scoring TextField and add it to the list.
+     *
      * @param w max width of the TextField
      * @return non-editable TextField
      */
@@ -68,6 +71,7 @@ public class YahtzeeGUI extends Application {
 
     /**
      * Create a scoring label and add it to the list.
+     *
      * @param n name of the label
      * @return the label
      */
@@ -77,13 +81,72 @@ public class YahtzeeGUI extends Application {
         return l;
     }
 
+    /**
+     * This sets prompts in the TextFields for values that could be used.
+     *
+     * @param s state - true means show prompts, false mean clear them
+     */
+    public void setPrompts(boolean s) {
+        if (!s) {
+            // clear them all
+            for (TextField tf : textFields)
+                tf.setPromptText("");
+        } else {
+            int[] t = Arrays.copyOf(dice, dice.length);
+            Arrays.sort(t);
+            for (Button b : buttons) {
+                if (!b.isDisabled()) {
+                    if (b == onesBTN) {
+                        onesTF.setPromptText("" + totalOf(t, 1));
+                    } else if (b == twosBTN) {
+                        twosTF.setPromptText("" + totalOf(t, 2));
+                    } else if (b == threesBTN) {
+                        threesTF.setPromptText("" + totalOf(t, 3));
+                    } else if (b == foursBTN) {
+                        foursTF.setPromptText("" + totalOf(t, 4));
+                    } else if (b == fivesBTN) {
+                        fivesTF.setPromptText("" + totalOf(t, 5));
+                    } else if (b == sixesBTN) {
+                        sixesTF.setPromptText("" + totalOf(t, 6));
+                    } else if (b == threeOKBTN) {
+                        threeOKTF.setPromptText(is3OfAKind(t) ? diceTotalTF.getText() : "");
+                    } else if (b == fourOKBTN) {
+                        fourOKTF.setPromptText(is4OfAKind(t) ? diceTotalTF.getText() : "");
+                    } else if (b == fullHouseBTN) {
+                        fullHouseTF.setPromptText(isFullHouse(t) ? "25" : "");
+                    } else if (b == smStrBTN) {
+                        smStrTF.setPromptText(isSmallStraight(t) ? "30" : "");
+                    } else if (b == lgStrBTN) {
+                        lgStrTF.setPromptText(isLargeStraight(t) ? "40" : "");
+                    } else if (b == yahtzeeBTN) {
+                        yahtzeeTF.setPromptText(isYahtzee(t) ? "50" : "");
+                    } else if (b == chanceBTN) {
+                        chanceTF.setPromptText(diceTotalTF.getText());
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Use the button state to determine if we've used all values.
+     *
+     * @return true if the game is over
+     */
+    public boolean isGameOver() {
+        for (Button b : buttons)
+            if (!b.isDisabled())
+                return false;
+        return true;
+    }
+
     public void createObjects() {
 
         final int minWidth = 100;
 
-        for (int x = 0 ; x < 5; x++) {
+        for (int x = 0; x < 5; x++) {
             diceTB[x] = new ToggleButton("");
-            diceTB[x].setMinWidth(minWidth/3);
+            diceTB[x].setMinWidth(minWidth / 3);
         }
 
         // everything related to the dice!
@@ -91,20 +154,23 @@ public class YahtzeeGUI extends Application {
         diceTotalTF = scoreTextField(minWidth);
         rollsLeftLBL = scoreLabel("Rolls Left:");
         rollsLeftTF = scoreTextField(minWidth);
-        rollBTN = scoreButton("Roll", minWidth/2);
+        rollBTN = scoreButton("Roll", minWidth / 2);
 
         // scoring pane - upper section buttons
         onesBTN = scoreButton("1's", minWidth);
-        twosBTN = scoreButton("2's",minWidth);
-        threesBTN = scoreButton("3's",minWidth);
-        foursBTN = scoreButton("4's",minWidth);
-        fivesBTN = scoreButton("5's",minWidth);
-        sixesBTN = scoreButton("6's",minWidth);
+        twosBTN = scoreButton("2's", minWidth);
+        threesBTN = scoreButton("3's", minWidth);
+        foursBTN = scoreButton("4's", minWidth);
+        fivesBTN = scoreButton("5's", minWidth);
+        sixesBTN = scoreButton("6's", minWidth);
 
         // scoring pane - upper section labels
-        subtotalLBL = scoreLabel("Subtotal:"); GridPane.setHalignment(subtotalLBL, HPos.RIGHT);
-        bonusLBL = scoreLabel("Bonus:"); GridPane.setHalignment(bonusLBL, HPos.RIGHT);
-        upperTotalLBL = scoreLabel("Upper Total:"); GridPane.setHalignment(upperTotalLBL, HPos.RIGHT);
+        subtotalLBL = scoreLabel("Subtotal:");
+        GridPane.setHalignment(subtotalLBL, HPos.RIGHT);
+        bonusLBL = scoreLabel("Bonus:");
+        GridPane.setHalignment(bonusLBL, HPos.RIGHT);
+        upperTotalLBL = scoreLabel("Upper Total:");
+        GridPane.setHalignment(upperTotalLBL, HPos.RIGHT);
 
         // scoring pane - upper section test fields
         onesTF = scoreTextField(minWidth);
@@ -127,7 +193,8 @@ public class YahtzeeGUI extends Application {
         chanceBTN = scoreButton("Chance", minWidth);
 
         // scoring pane - lower section labels
-        totalLBL = scoreLabel("Total Score"); GridPane.setHalignment(totalLBL, HPos.RIGHT);
+        totalLBL = scoreLabel("Total Score");
+        GridPane.setHalignment(totalLBL, HPos.RIGHT);
 
         // scoring pane - lower section text fields
         threeOKTF = scoreTextField(minWidth);
@@ -142,15 +209,17 @@ public class YahtzeeGUI extends Application {
 
     /**
      * Sum the total of dice with value of dtype.
+     *
+     * @param d sorted dice set.
      * @param dtype the die value to sum.
      * @return int as the sum of dice matching dtype.
      */
-    public int totalOf(int dtype) {
+    public int totalOf(int[] d, int dtype) {
         int count = 0;
 
         // count the dtype dice
         for (int x = 0; x < 5; x++)
-            if (dice[x] == dtype)
+            if (d[x] == dtype)
                 count++;
 
         // number of dice times dtype
@@ -163,31 +232,34 @@ public class YahtzeeGUI extends Application {
     private class ButtonHandler implements EventHandler<ActionEvent> {
         @Override
         public void handle(ActionEvent e) {
-            Button b = (Button)e.getSource();
+            Button b = (Button) e.getSource();
             boolean off = true;
             boolean r = false; // do I need to reset?
 
             // if it's not the rollBTN, might as well sort them.
             // we're done here...
-            if (b != rollBTN)
+            if (b != rollBTN) {
                 Arrays.sort(dice);
+                setPrompts(false);
+            }
 
             if (b == rollBTN) {
-                if ( rolls > 0 )
+                if (rolls > 0)
                     roll();
+                setPrompts(true);
                 off = false;
             } else if (b == onesBTN) {
-                onesTF.setText("" + totalOf(1));
+                onesTF.setText("" + totalOf(dice, 1));
             } else if (b == twosBTN) {
-                twosTF.setText("" + totalOf(2));
+                twosTF.setText("" + totalOf(dice, 2));
             } else if (b == threesBTN) {
-                threesTF.setText("" + totalOf(3));
+                threesTF.setText("" + totalOf(dice, 3));
             } else if (b == foursBTN) {
-                foursTF.setText("" + totalOf(4));
+                foursTF.setText("" + totalOf(dice, 4));
             } else if (b == fivesBTN) {
-                fivesTF.setText("" + totalOf(5));
+                fivesTF.setText("" + totalOf(dice, 5));
             } else if (b == sixesBTN) {
-                sixesTF.setText("" + totalOf(6));
+                sixesTF.setText("" + totalOf(dice, 6));
             } else if (b == threeOKBTN) {
                 threeOKTF.setText(is3OfAKind(dice) ? diceTotalTF.getText() : "0");
             } else if (b == fourOKBTN) {
@@ -204,6 +276,7 @@ public class YahtzeeGUI extends Application {
                 chanceTF.setText(diceTotalTF.getText());
             }
 
+            // change disable state.
             b.setDisable(off);
             if (b != rollBTN) {
                 resetDice();
@@ -218,7 +291,7 @@ public class YahtzeeGUI extends Application {
      */
     public void resetDice() {
         rolls = 3;
-        for (int x = 0 ; x < 5; x++) {
+        for (int x = 0; x < 5; x++) {
             dice[x] = 0;
             diceTB[x].setSelected(false);
         }
@@ -232,7 +305,7 @@ public class YahtzeeGUI extends Application {
         GridPane detailPane = new GridPane();
         GridPane pane = new GridPane();
         GridPane dicePane = new GridPane();
-        
+
         // And then our main attraction. The VBox holds everything!
         VBox mainView = new VBox(detailPane, dicePane, pane);
         Scene scene = new Scene(mainView, 500, 750);
@@ -247,49 +320,64 @@ public class YahtzeeGUI extends Application {
         detailPane.setHgap(5);
         detailPane.setVgap(5);
         detailPane.setAlignment(Pos.CENTER);
-        detailPane.add(rollsLeftLBL, 0, 1); detailPane.add(rollsLeftTF, 1, 1);
-        detailPane.add(diceTotalLBL, 0, 2); detailPane.add(diceTotalTF, 1, 2);
+        detailPane.add(rollsLeftLBL, 0, 1);
+        detailPane.add(rollsLeftTF, 1, 1);
+        detailPane.add(diceTotalLBL, 0, 2);
+        detailPane.add(diceTotalTF, 1, 2);
 
         // dice pane
         dicePane.setHgap(5);
         dicePane.setVgap(5);
         dicePane.setAlignment(Pos.CENTER);
         dicePane.add(rollBTN, 8, 5);
-        for ( int x = 0; x < 5; x++ )
+        for (int x = 0; x < 5; x++)
             dicePane.add(diceTB[x], x, 5);
         // scoring pane - upper section
         pane.setHgap(5);
         pane.setVgap(5);
         pane.setAlignment(Pos.CENTER);
-        pane.add(onesBTN, 0, 4); pane.add(onesTF, 1, 4);
-        pane.add(twosBTN, 0, 5); pane.add(twosTF, 1, 5);
-        pane.add(threesBTN, 0, 6); pane.add(threesTF, 1, 6);
-        pane.add(foursBTN, 0, 7); pane.add(foursTF, 1, 7);
-        pane.add(fivesBTN, 0, 8); pane.add(fivesTF, 1, 8);
-        pane.add(sixesBTN, 0, 9); pane.add(sixesTF, 1, 9);
+        pane.add(onesBTN, 0, 4);
+        pane.add(onesTF, 1, 4);
+        pane.add(twosBTN, 0, 5);
+        pane.add(twosTF, 1, 5);
+        pane.add(threesBTN, 0, 6);
+        pane.add(threesTF, 1, 6);
+        pane.add(foursBTN, 0, 7);
+        pane.add(foursTF, 1, 7);
+        pane.add(fivesBTN, 0, 8);
+        pane.add(fivesTF, 1, 8);
+        pane.add(sixesBTN, 0, 9);
+        pane.add(sixesTF, 1, 9);
 
-        pane.add(subtotalLBL, 0, 10); pane.add(subtotalTF, 1, 10);
-        pane.add(bonusLBL, 0, 11); pane.add(bonusTF, 1, 11);
-        pane.add(upperTotalLBL, 0, 12); pane.add(upperTotalTF, 1, 12);
+        pane.add(subtotalLBL, 0, 10);
+        pane.add(subtotalTF, 1, 10);
+        pane.add(bonusLBL, 0, 11);
+        pane.add(bonusTF, 1, 11);
+        pane.add(upperTotalLBL, 0, 12);
+        pane.add(upperTotalTF, 1, 12);
 
         // lower section
-        pane.add(threeOKBTN, 2, 13); pane.add(threeOKTF, 3, 13);
-        pane.add(fourOKBTN, 2, 14); pane.add(fourOKTF, 3, 14);
-        pane.add(fullHouseBTN, 2, 15); pane.add(fullHouseTF, 3, 15);
-        pane.add(smStrBTN, 2, 16); pane.add(smStrTF, 3, 16);
-        pane.add(lgStrBTN, 2, 17); pane.add(lgStrTF, 3, 17);
-        pane.add(yahtzeeBTN, 2, 18); pane.add(yahtzeeTF, 3, 18);
-        pane.add(chanceBTN, 2, 19); pane.add(chanceTF, 3, 19);
-        pane.add(totalLBL, 2, 20); pane.add(totalTF, 3, 20);
-        // tfdst.setEditable(false);
+        pane.add(threeOKBTN, 2, 13);
+        pane.add(threeOKTF, 3, 13);
+        pane.add(fourOKBTN, 2, 14);
+        pane.add(fourOKTF, 3, 14);
+        pane.add(fullHouseBTN, 2, 15);
+        pane.add(fullHouseTF, 3, 15);
+        pane.add(smStrBTN, 2, 16);
+        pane.add(smStrTF, 3, 16);
+        pane.add(lgStrBTN, 2, 17);
+        pane.add(lgStrTF, 3, 17);
+        pane.add(yahtzeeBTN, 2, 18);
+        pane.add(yahtzeeTF, 3, 18);
+        pane.add(chanceBTN, 2, 19);
+        pane.add(chanceTF, 3, 19);
+        pane.add(totalLBL, 2, 20);
+        pane.add(totalTF, 3, 20);
 
         System.out.println(pane);
         primaryStage.setTitle("Button Event Inner Class");
         primaryStage.setScene(scene);
         primaryStage.show();
-
-
-        // YEAH! NOW THINGS BEGIN TO HAPPEN!
 
         showDice();
     }
@@ -299,9 +387,9 @@ public class YahtzeeGUI extends Application {
      */
     public void roll() {
 
-        for ( int x = 0; x < 5; x++ ) {
-            if ( rolls == 3 || !diceTB[x].isSelected())
-                dice[x] = (int)(Math.random() * 6 + 1);
+        for (int x = 0; x < 5; x++) {
+            if (rolls == 3 || !diceTB[x].isSelected())
+                dice[x] = (int) (Math.random() * 6 + 1);
         }
         rolls--;
         showDice();
@@ -330,7 +418,7 @@ public class YahtzeeGUI extends Application {
      */
     public void updateDiceTotal() {
         int sum = 0;
-        for ( int x = 0; x < 5; x++ )
+        for (int x = 0; x < 5; x++)
             sum = sum + dice[x];
         diceTotalTF.setText("" + sum);
     }
@@ -341,7 +429,7 @@ public class YahtzeeGUI extends Application {
      * strings and ints everywhere.
      */
     public void updateUpperTotal() {
-        int sum = 0;
+        int sum;
         // if button is disabled, something was written in the TF
         sum = (onesBTN.isDisabled() ? Integer.parseInt(onesTF.getText()) : 0)
                 + (twosBTN.isDisabled() ? Integer.parseInt(twosTF.getText()) : 0)
@@ -351,12 +439,15 @@ public class YahtzeeGUI extends Application {
                 + (sixesBTN.isDisabled() ? Integer.parseInt(sixesTF.getText()) : 0);
 
         subtotalTF.setText("" + sum);
-        bonusTF.setText( sum >= 63 ? "35" : "0");
-        upperTotalTF.setText(""+ (sum + Integer.parseInt(bonusTF.getText())));
+        bonusTF.setText(sum >= 63 ? "35" : "0");
+        upperTotalTF.setText("" + (sum + Integer.parseInt(bonusTF.getText())));
     }
 
+    /**
+     * Update total calling updateUpperTotal().
+     */
     public void updateTotal() {
-        int sum = 0;
+        int sum;
 
         updateUpperTotal();
         sum = (threeOKBTN.isDisabled() ? Integer.parseInt(threeOKTF.getText()) : 0)
@@ -371,87 +462,96 @@ public class YahtzeeGUI extends Application {
 
     /**
      * Determine if the first four dice in the sorted array are sequential.
+     *
      * @param d as the array of dice
      * @return true if it's a lower straight.
      */
-    public static boolean isLowerStraight (int[] d) {
+    public static boolean isLowerStraight(int[] d) {
         int v = d[0];
-        for (int x = 1; x <=3; x++)
-            if (d[x] != v+x)
+        for (int x = 1; x <= 3; x++)
+            if (d[x] != v + x)
                 return false;
         return true;
     }
 
     /**
      * Determine if the last four dice in the sorted array are sequential.
+     *
      * @param d as the array of dice.
      * @return true if it's an upper straight.
      */
-    public static boolean isUpperStraight (int[] d) {
+    public static boolean isUpperStraight(int[] d) {
         int v = d[1];
-        for (int x = 2; x <=4; x++)
-            if (d[x] != v+x-1)
+        for (int x = 2; x <= 4; x++)
+            if (d[x] != v + x - 1)
                 return false;
         return true;
     }
 
     /**
      * Determine if the sorted array of dice is a small straight.
+     *
      * @param d is the array of dice
      * @return true if it's a small straight
      */
-    public static boolean isSmallStraight (int[] d) {
+    public static boolean isSmallStraight(int[] d) {
+        // TODO: Does not properly detect 3 4 4 5 6 as small straight!
         return isLowerStraight(d) || isUpperStraight(d);
     }
 
     /**
      * Determine if the sorted array of dice is a large straight
+     *
      * @param d as the array of dice.
      * @return true if all values are sequential.
      */
-    public static boolean isLargeStraight (int[] d) {
+    public static boolean isLargeStraight(int[] d) {
         int v = d[0];
-        for (int x = 1; x <=4; x++)
-            if (d[x] != v+x)
+        for (int x = 1; x <= 4; x++)
+            if (d[x] != v + x)
                 return false;
         return true;
     }
 
     /**
      * Determine if the sorted array of dice is a fill house.
+     *
      * @param d as the array of dice.
      * @return true if the patter xxyyy or xxxyy can be discerned.
      */
-    public static boolean isFullHouse (int[] d) {
-        return ( (d[0] != d[4]) &&
-                ( ((d[0] == d[1]) && (d[2] == d[4])) ||
-                        ((d[0] == d[2]) && (d[3] == d[4])) ) );
+    public static boolean isFullHouse(int[] d) {
+        return ((d[0] != d[4]) &&
+                (((d[0] == d[1]) && (d[2] == d[4])) ||
+                        ((d[0] == d[2]) && (d[3] == d[4]))));
     }
 
     /**
      * Determine if the sorted array is 3 of a kind
+     *
      * @param d as the sorted array of dice
      * @return true if 1&3, 2&4, or 3&5 match.
      */
-    public static boolean is3OfAKind (int[] d) {
+    public static boolean is3OfAKind(int[] d) {
         return (d[0] == d[2]) || (d[1] == d[3]) || (d[2] == d[4]);
     }
 
     /**
      * Determine if the sorted array is 4 of a kind
+     *
      * @param d as the sorted array of dice.
      * @return true if 1&4 or 2&5 match.
      */
-    public static boolean is4OfAKind (int[] d) {
+    public static boolean is4OfAKind(int[] d) {
         return (d[0] == d[3]) || (d[1] == d[4]);
     }
 
     /**
      * Determine if the sorted array is 5 of a kind
+     *
      * @param d as the sorted array of dice.
      * @return true if 1&5 match.
      */
-    public static boolean isYahtzee (int[] d) {
+    public static boolean isYahtzee(int[] d) {
         return d[0] == d[4];
     }
 
